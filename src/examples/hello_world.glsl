2e8f60dcd39895f8a89d04f9c37d18fd147ecc49
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2014-07-30 10:45:42 +0200

    less volume in hello_world + polish sweet_dream

--- src/examples/hello_world.glsl
+++ src/examples/hello_world.glsl
@@ -6,7 +6,7 @@ float PI = 3.14159;
  * returned value: a sample in [ -1, 1 ] range.
  */
 float dsp (float t) {
-  return sin(2.0 * PI * t * 440.0); // The most fundamental sound: a sine wave at 440 Hz
+  return 0.4 * sin(2.0 * PI * t * 440.0); // The most fundamental sound: a sine wave at 440 Hz
 }
 
 // Notes:
